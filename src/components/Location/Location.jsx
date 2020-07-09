f88d39c68dac8ddd9b617577e05e68d5a2c87b32
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-07-10 00:42:36 +0300

    refactor: remove useless variable

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import './Location.css';
 
-export const Location = ({ city, country }) => {
-  const r = '';
-  return (
-    <div className="location">
-      <span className="location__name">{`${city}, ${country}`}</span>
-    </div>
-  );
-};
+export const Location = ({ city, country }) => (
+  <div className="location">
+    <span className="location__name">{`${city}, ${country}`}</span>
+  </div>
+);
