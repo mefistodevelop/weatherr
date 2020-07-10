c6f2aa08343bd185bf089d2eda930b3783358014
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-07-10 13:20:00 +0300

    Updates

---
+++ precache-manifest.53efcbf547e974f56529713df3ed8343.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba80114058d110bb2a52b3ad4f553d48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2540dd991b219159457f",
+    "url": "/static/css/main.a7956e76.chunk.css"
+  },
+  {
+    "revision": "eda29c7d7932e91af780",
+    "url": "/static/js/2.6a4ed3e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a4ed3e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2540dd991b219159457f",
+    "url": "/static/js/main.4b114de3.chunk.js"
+  },
+  {
+    "revision": "49af3018cb0e3c753c5a",
+    "url": "/static/js/runtime-main.5e8f0e8c.js"
+  },
+  {
+    "revision": "b101b48666f093a4777d8daa3084fcdb",
+    "url": "/static/media/spinner.b101b486.svg"
+  }
+]);
