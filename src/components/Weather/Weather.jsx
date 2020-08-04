2b9d1f878235d005506775991cc4611b6d57f946
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-08-04 15:40:06 +0300

    feat: add localization to weather info

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './Weather.css';
+import { useSelector } from 'react-redux';
 
 export const Weather = ({ temperature, weather, wind }) => {
+  const language = useSelector((state) => state.language);
+
   const getSign = (temp) => {
     let sign = '';
     if (temp > 0) {
@@ -26,17 +29,34 @@ export const Weather = ({ temperature, weather, wind }) => {
     }
   };
 
+  const locals = {
+    feelsLike: {
+      en: 'feels like',
+      ru: 'ощущается как',
+    },
+    wind: {
+      en: 'wind',
+      ru: 'ветер',
+    },
+    speedUnits: {
+      en: 'm/s',
+      ru: 'м/с',
+    },
+  };
+
   return (
     <div className="weather">
       <div className="weather__info">
         {`${getSign(temperature.real)}${temperature.real}°C`}
         <div className="weather__description">{weather.description}</div>
         <div className="weather__description">
-          {`feels like ${getSign(temperature.feelsLike)}${
+          {`${locals.feelsLike[language]} ${getSign(temperature.feelsLike)}${
             temperature.feelsLike
           }°C`}
         </div>
-        <div className="weather__description">{`wind ${wind} m/s`}</div>
+        <div className="weather__description">
+          {`${locals.wind[language]} ${wind} ${locals.speedUnits[language]}`}
+        </div>
       </div>
       <div className="weather__icon">
         <span className={getIconClass()} />
